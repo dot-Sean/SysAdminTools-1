270ba3c313d17ba1743f468c3865d7a301fc32c7
draik <draikx21@gmail.com>
2013-12-16 11:10:11 -0800

    newip.py -- added file check and SMS notice

--- newip.py
+++ newip.py
@@ -8,7 +8,8 @@
 # 'from_' is your Twilio phone number, selected at sign-up.
 
 import logging
-import socket
+from os.path import isfile
+from socket import gethostname
 from twilio.rest import TwilioRestClient
 import urllib2
 
@@ -29,16 +30,24 @@ account_sid = '<Twilio Account SID>'
 auth_token = '<Twilio Authentication Token>'
 client = TwilioRestClient(account_sid, auth_token)
 
-my_host = socket.gethostname()
-saved_ip = '~/.twilio/ip'
-with open(saved_ip, 'r') as old_ip:
-    cur_ip = old_ip.read()
-    if cur_ip == my_ip:
-        log.info("IP address is the same -- %s", my_ip)
-    else:
-        log.info("IP address has changed -- %s", my_ip)
-        with open(saved_ip, 'w') as new_ip:
-            new_ip.write(my_ip)
-        sms = client.messages.create(to='+1<your_phone#>',
-                                    from_='+1<twilio_phone#>',
-                                    body=my_host+": "+my_ip)
+my_host = gethostname()
+ip_file = '~/.twilio/ip'
+if isfile(ip_file):
+    with open(ip_file, 'r') as old_ip:
+        cur_ip = old_ip.read()
+        if cur_ip == my_ip:
+            log.info("IP address is the same -- %s", my_ip)
+        else:
+            log.info("IP address has changed -- %s", my_ip)
+            with open(ip_file, 'w') as new_ip:
+                new_ip.write(my_ip)
+            sms = client.messages.create(to='+1<your_phone#>',
+                                        from_='+1<twilio_phone#>',
+                                        body="New IP: "+my_host+": "+my_ip)
+else:
+    with open(ip_file, 'w') as save_ip:
+        save_ip.write(my_ip)
+    log.info("IP address established -- %s", my_ip)
+    sms = client.messages.create(to='+1<your_phone#>',
+                                from_='+1<twilio_phone#>',
+                                body="Set IP: "+my_host+": "+my_ip)
